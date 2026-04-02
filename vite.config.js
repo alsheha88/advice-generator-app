600004d3841ce830bd45460db61bffbdc981bb72
Abdulaziz <alsheha88@gmail.com>
2026-04-02 22:47:37 +0300

    github fixes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/advice-generator/',
+   base: '/advice-generator-app/',
   plugins: [react(), tailwindcss()],
 })
